273f48fe3890ccc817e75cd0775f1826e0d28202
Tai Phan <taip26@vt.edu>
2024-04-11 15:50:34 -0400

    Fixed error with run.sh path

--- run.sh
+++ run.sh
@@ -1,3 +1,2 @@
 PARENT=$HOME
-(cd /root/home/Projects/Personal/rpi_clock/bin; python3 rpi_clock.py rpi_clock.cfg 2>&1 | tee /root/home/Projects/Personal/rpi_clock/rpi_clock.log) &
-
+(cd $PARENT/rpi_clock/bin; python3 rpi_clock.py rpi_clock.cfg 2>&1 | tee $PARENT/rpi_clock/rpi_clock.log) &
